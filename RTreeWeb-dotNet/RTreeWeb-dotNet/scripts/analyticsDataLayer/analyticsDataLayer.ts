461271321a6d072aac56988cccaefdb3d605ed3a
David Sergio <davidsergio52@gmail.com>
2020-07-21 18:18:03 -0700

    added analytics data layer

---
+++ RTreeWeb-dotNet/RTreeWeb-dotNet/scripts/analyticsDataLayer/analyticsDataLayer.ts
@@ -0,0 +1,166 @@
+﻿
+
+
+export class Telemetry {
+    config: TelemetryConfig;
+    events: IEventDictionary = {};
+    pages: IPageDictionary = {};
+    size: number = 0;
+
+    constructor(config: TelemetryConfig) {
+        this.config = config;
+    }
+
+    addEvent(event: ITelementryEvent): void {
+        this.events[event.getId()] = event;
+        this.size++;
+    }
+
+    getEvent(eventId: string): ITelementryEvent {
+        return this.events[eventId];
+    }
+
+    addPage(page: TelemetryPage): void {
+        this.pages[page.id] = page;
+    }
+
+    getPage(pageId: string): TelemetryPage {
+        return this.pages[pageId];
+    }
+
+    getEventsCount(): number {
+        return this.size;
+    }
+}
+
+abstract class TelemetryConfig {
+    type: string;
+
+    constructor(type: string) {
+        this.type = type;
+    }
+}
+
+export interface IEventDictionary {
+    [key: string]: ITelementryEvent;
+};
+export interface IPageDictionary {
+    [key: string]: TelemetryPage;
+};
+
+
+export class TelemetryConfigGA extends TelemetryConfig {
+    tid: string;
+
+    constructor(tid: string) {
+        super("GA");
+        this.tid = tid;
+    }
+}
+
+
+
+
+
+abstract class TelemetryPage {
+    id: string;
+    pageName: string;
+
+    constructor(pageName: string, id: string) {
+        this.pageName = pageName;
+        this.id = id;
+    }
+
+    getPageName(): string {
+        return this.pageName;
+    }
+}
+
+export class TelemetryPageGA extends TelemetryPage {
+
+    constructor(pageName: string, id: string) {
+        super(pageName, id);
+    }
+}
+
+
+
+
+
+
+
+export interface ITelementryEvent {
+    
+    displayEvent: () => void;
+    getId(): string;
+    getTelemetryEventAttribute: (i: number) => string;
+    getTelementryEventAttributeCount(): number;
+}
+
+abstract class TelemetryEvent implements ITelementryEvent {
+    id: string;
+    type: string;
+
+    constructor(type: string, id: string) {
+        this.type = type;
+        this.id = id;
+    }
+
+    getId(): string {
+        return this.id
+    }
+
+    abstract displayEvent(): void;
+    abstract getTelemetryEventAttribute(i: number): string;
+    abstract getTelementryEventAttributeCount(): number;
+}
+
+export class TelemetryEventGA extends TelemetryEvent {
+    eventCategory: string;
+    eventAction: string;
+    eventLabel?: string;
+
+ 
+    constructor(id: string, eventCategory: string, eventAction: string, eventLabel: string) {
+        super("GA", id);
+        this.eventCategory = eventCategory;
+        this.eventAction = eventAction;
+        this.eventLabel = eventLabel;
+    }
+
+    
+    getTelementryEventAttributeCount(): number {
+        return 3;
+    }
+
+    displayEvent(): void {
+        console.log("id: " + this.id);
+        console.log("type:" + this.type);
+        console.log("eventCategory: " + this.eventCategory);
+        console.log("eventAction: " + this.eventAction);
+        if (this.eventLabel != null) {
+            console.log("eventLabel: " + this.eventLabel);
+        } else {
+            console.log("eventLabel: " + "not set");
+        }
+    }
+
+    getTelemetryEventAttribute(i: number): string {
+        switch (i) {
+            case 0: {
+                return this.eventCategory;
+            }
+            case 1: {
+                return this.eventAction;
+            }
+            case 2: {
+                if (this.eventLabel != null) {
+                    return this.eventLabel;
+                } else {
+                    return "";
+                }
+            }
+        }
+    }
+}
+
--- RTreeWeb-dotNet/RTreeWeb-dotNet/scripts/components/RTree/rtreeComponentList.vue
+++ RTreeWeb-dotNet/RTreeWeb-dotNet/scripts/components/RTree/rtreeComponentList.vue
@@ -15,7 +15,7 @@
                     <td>{{t.name}}</td>
                     <td>{{t.numDimensions}}</td>
                     <td>
-                        <button class="button is-primary" @click="edit(t)">Show/Edit</button>
+                        <button data-telemetry-action :data-telemetry-action-id="t.name" v-bind="telemetryActionAttributes(t)" class="button is-primary" @click="edit(t)">Show/Edit</button>
                         <!--<button class="button" @click="deleteTree(t)">Delete</button>-->
 
                     </td>
@@ -37,6 +37,8 @@
     import { RTree, RTreeClient } from '../../api-client.g';
     import rtreeComponentDetail from './rtreeComponentDetail.vue';
 
+    import { Telemetry, TelemetryConfigGA, TelemetryEventGA, TelemetryPageGA, ITelementryEvent } from '../../analyticsDataLayer/analyticsDataLayer';
+
     declare var apiUrl: string;
 
     //apiUrl = "http://dsergio-rtree-api-boot.azurewebsites.net";
@@ -54,6 +56,8 @@
         rtreeClient: RTreeClient;
         isLoading: boolean = false;
 
+        telemetry: Telemetry;
+
         constructor() {
             super();
             this.rtreeClient = new RTreeClient(apiUrl);
@@ -61,7 +65,51 @@
 
         async mounted() {
             this.trees = await this.rtreeClient.getAll();
+            this.initAnalyticsDataLayer();
             console.log("trees: ", this.trees);
+        }
+
+        initAnalyticsDataLayer(): void {
+
+            let telemetryConfigGA: TelemetryConfigGA = new TelemetryConfigGA("UA-173118780-1");
+            this.telemetry = new Telemetry(telemetryConfigGA);
+
+            let page: TelemetryPageGA = new TelemetryPageGA("rtree app home", "123");
+            this.telemetry.addPage(page);
+
+            for (var i in this.trees) {
+                let eventShow: ITelementryEvent = new TelemetryEventGA(this.trees[i].name, "rtree app", "show/edit", this.trees[i].name);
+                this.telemetry.addEvent(eventShow);
+            }
+            
+        }
+
+        telemetryActionAttributes(t: RTree): object {
+            console.log("telemetry attr: ", t);
+            let i: number = 0;
+            let obj: any = {};
+            let id = t.name;
+            if (this.telemetry.getEvent(id) != null) {
+                for (i = 0; i < this.telemetry.getEvent(id).getTelementryEventAttributeCount(); i++) {
+                    let str: string = "data-telemetry-attribute-" + i;
+                    obj[str] = this.telemetry.getEvent(id).getTelemetryEventAttribute(i);
+                }
+            }
+            
+            return obj;
+        }
+
+        telemetryActionAttributes2(id: string): object {
+            let i: number = 0;
+            let obj: any = {};
+            if (this.telemetry.getEvent(id) != null) {
+                for (i = 0; i < this.telemetry.getEvent(id).getTelementryEventAttributeCount(); i++) {
+                    let str: string = "data-telemetry-attribute-" + i;
+                    obj[str] = this.telemetry.getEvent(id).getTelemetryEventAttribute(i);
+                }
+            }
+            
+            return obj;
         }
 
         async edit(tree: RTree) {
